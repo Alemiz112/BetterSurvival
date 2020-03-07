69aa7aedd50b7069a66602aa7c0787263ed83da7
Alemiz112 <45739737+Alemiz112@users.noreply.github.com>
2020-03-07 15:33:08 +0100

    MiltiWorld is now supported by MyLandProtect

--- src/main/java/alemiz/bettersurvival/addons/myland/LandRegion.java
+++ src/main/java/alemiz/bettersurvival/addons/myland/LandRegion.java
@@ -1,6 +1,8 @@
 package alemiz.bettersurvival.addons.myland;
 
 import alemiz.bettersurvival.utils.ConfigManager;
+import cn.nukkit.Server;
+import cn.nukkit.level.Level;
 import cn.nukkit.math.Vector3f;
 import cn.nukkit.utils.Config;
 
@@ -19,12 +21,22 @@ public class LandRegion {
 
     public List<String> whitelist = new ArrayList<>();
 
+    public Level level = null;
     public Vector3f pos1;
     public Vector3f pos2;
 
     public LandRegion(String owner, String name){
+        this(owner, name, null);
+    }
+
+    public LandRegion(String owner, String name, Level level){
         this.owner = owner;
         this.land = name;
+
+        if (level == null){
+            level = Server.getInstance().getDefaultLevel();
+        }
+        this.level = level;
     }
 
     public void addWhitelist(String player){
@@ -41,6 +53,7 @@ public class LandRegion {
         Config config = ConfigManager.getInstance().loadPlayer(owner);
         if (config == null) return;
 
+        config.set("land."+land.toLowerCase()+".level", level.getFolderName().toLowerCase());
         config.set("land."+land.toLowerCase()+".pos0", new float[]{pos1.getX(), pos1.getY(), pos1.getZ()});
         config.set("land."+land.toLowerCase()+".pos1", new float[]{pos2.getX(), pos2.getY(), pos2.getZ()});
 
